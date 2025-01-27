831f39c4f22f6ce350ea45af848eab8c37e4fcfe
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-01-27 17:52:50 +0530

    ui changes

--- Shopping_App/shop_app/lib/home_page.dart
+++ Shopping_App/shop_app/lib/home_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget { //convert to state full so we can get the state changes 
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const['All', 'Addidas', 'Nike', 'Bata'];
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +19,7 @@ class HomePage extends StatelessWidget {
                         borderRadius: BorderRadius.horizontal(left:Radius.circular(50) )
                       );
   
-    return const Scaffold(
+    return  Scaffold(
       body: SafeArea(
         //safe area will ignore the area of notches of mobile phone
         child: Column(
@@ -35,8 +42,37 @@ class HomePage extends StatelessWidget {
                       enabledBorder: border,
                       focusedBorder: border
                   ))
-                )
+                ),
+
+              
               ],
+            ),
+            SizedBox(
+              height: 120,
+               child: ListView.builder( //to display the list items || this will try to take all high and width so use sized box
+                    itemCount: filters.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder:(context, index) {
+                      final filter = filters[index];
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 8, right: 8),
+                        child: Chip(
+                          label: Text(filter),
+                          backgroundColor: Color.fromRGBO(245, 247, 249, 1),
+                          side: const BorderSide(
+                            color: Color.fromRGBO(245, 247, 249, 1)
+                          ),
+                          labelStyle: TextStyle(
+                          fontSize: 16
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                        shape:RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        ) ,
+                        ),
+                      );
+                    },
+                    ),
             )
           ],
         ),
