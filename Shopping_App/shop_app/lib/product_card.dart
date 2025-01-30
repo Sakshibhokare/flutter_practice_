1fc3473a8dd3d26cffe2d0f106c37c180ba75edb
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-01-30 12:48:43 +0530

    card color updated

--- Shopping_App/shop_app/lib/product_card.dart
+++ Shopping_App/shop_app/lib/product_card.dart
@@ -4,17 +4,23 @@ class ProductCard extends StatelessWidget {
   final String title;
   final double price;
   final String image;
+  final Color backgroundColor;
   const ProductCard({
     super.key,
     required this.title,
     required this.price,
-    required this.image});
+    required this.image,
+    required this.backgroundColor});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color.fromRGBO(216, 240, 253, 1),
+      
       margin: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(20)
+      ),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
