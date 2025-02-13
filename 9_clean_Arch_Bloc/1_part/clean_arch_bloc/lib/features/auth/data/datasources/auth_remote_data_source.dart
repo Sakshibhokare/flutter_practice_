621f32c8101d53666c7b50c9c1d9cbeaf59b0463
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-02-13 19:24:37 +0530

    auth remote data source

--- 9_clean_Arch_Bloc/1_part/clean_arch_bloc/lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ 9_clean_Arch_Bloc/1_part/clean_arch_bloc/lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -1,6 +1,8 @@
-//remote data source means data is coming from the APIs 
+//remote data source means data is coming from the APIs
 
 //creating an interface for auth_remote_data_source
+
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class AuthRemoteDataSource {
   Future<String> signUpWithEmailPassword({
@@ -15,18 +17,23 @@ abstract interface class AuthRemoteDataSource {
   });
 }
 
-//general class that will implement this interface 
-class AuthRemoteDataSourceImpl implements AuthRemoteDataSource{
+//general class that will implement this interface
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  //this is the dependency injection: this will decrease the dependency and create the loosely coupled
+  final SupabaseClient
+      supabaseClient; //doing this will create a dependency, any time when we change the database we can simply change the client instance
+
+  AuthRemoteDataSourceImpl(this.supabaseClient);
   @override
-  Future<String> logInWithEmailPassword({required String email, required String password}) {
+  Future<String> logInWithEmailPassword(
+      {required String email, required String password}) {
     // TODO: implement logInWithEmailPassword
     throw UnimplementedError();
   }
 
   @override
-  Future<String> signUpWithEmailPassword({required String name, required String email, required String password}) {
-    // TODO: implement signUpWithEmailPassword
-    throw UnimplementedError();
-  }
-
+  Future<String> signUpWithEmailPassword(
+      {required String name,
+      required String email,
+      required String password}) async {}
 }
