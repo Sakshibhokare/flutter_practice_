8b17e3536b0247c1548f704fa8d80d0f782b6556
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2025-02-18 18:45:31 +0530

    use cases

--- 9_clean_Arch_Bloc/1_part/clean_arch_bloc/lib/features/auth/domain/usecases/user_sign_up.dart
+++ 9_clean_Arch_Bloc/1_part/clean_arch_bloc/lib/features/auth/domain/usecases/user_sign_up.dart
@@ -1,1 +1,15 @@
-class UserSignUp {}
+import 'package:clean_arch_bloc/core/usecase/usecase.dart';
+
+class UserSignUp
+    implements
+        UseCase<String,
+            UserSignUpParams> {} //we create an interface for usecases will create a usecase interface in the core folder
+
+class UserSignUpParams {
+  //with single variable we can not pass multiple things so create a class and pass it
+  final String email;
+  final String password;
+  final String name;
+
+  UserSignUpParams(this.email, this.password, this.name);
+}
